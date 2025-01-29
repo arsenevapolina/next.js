23d7520962fff1efedc57420efc4ddef9df40542
Полина <pechenkova.polina.mgu@yandex.ru>
2025-01-29 14:22:05 +0300

    feature - implemented asynchronous request in the server component

---
+++ top-app/api/getPosts.ts
@@ -0,0 +1,11 @@
+import { Post } from "@/app/interfaces/Post";
+
+export async function getPosts(): Promise<Post[]> {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch posts");
+  }
+
+  return await res.json();
+}
--- top-app/app/components/Card/Card.props.ts
+++ top-app/app/components/Card/Card.props.ts
@@ -6,4 +6,5 @@ export interface CardProps
   text: string;
   time: number;
   likes: number;
+  postId: number;
 }
